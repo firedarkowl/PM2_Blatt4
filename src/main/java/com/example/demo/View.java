0c8cf816f0dc71f98ab5ed011f62fe1f44934cf5
firedarkowl <firedarkowl@gmail.com>
2023-06-08 22:14:05 +0200

    wird noch weiter gearbeitet

---
+++ src/main/java/com/example/demo/View.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import javafx.scene.control.TableView;
+
+import java.io.IOException;
+
+public class View {
+
+    private TableView<Eintrag> table;
+
+    public TableView getTable() throws IOException{
+
+    }
+}
